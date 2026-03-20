69c4709713feaced43c60e5255fa97e01746021c
yatendra121kushwaha <47475601+yatendra121kushwaha@users.noreply.github.com>
2026-03-20 22:30:20 +0530

    fix: vuetify/index.ts - fix icon import path for v4, remove dead code

--- src/plugins/vuetify/index.ts
+++ src/plugins/vuetify/index.ts
@@ -1,30 +1,14 @@
 // Styles
-//import 'vuetify/lib/styles/main.sass'
-//import 'vuetify/styles'
-//import 'vuetify/dist/vuetify.css'
 import '@/styles/_variables.scss'
-// Vuetify
 
+// Vuetify
 import { createVuetify, IconOptions } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-//import colors from 'vuetify/lib/util/colors'
 import themes from './theme'
 import defaultConfig from './defaultConfig'
-import { aliases, mdi } from 'vuetify/lib/iconsets/mdi-svg'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
 import { md3 } from 'vuetify/blueprints'
-
-console.log({ components })
-
-const defaultThemeOptions = {
-    defaultTheme: 'light',
-    variations: {
-        colors: [],
-        lighten: 0,
-        darken: 0
-    },
-    themes
-}
 
 const icons: IconOptions = {
     defaultSet: 'mdi',
